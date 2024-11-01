d75d35ea52a5025ed5ba57ec742946f886ee0648
ianhallWDJUN22 <ianmhall6@gmail.com>
2024-11-01 15:47:13 -0400

    presignedURL working, Get, post, working.

--- bin/music-library.ts
+++ bin/music-library.ts
@@ -11,7 +11,7 @@ new MusicLibraryStack(app, 'MusicLibraryStack', {
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  env: { account: '212879372600', region: 'us-east-1' },
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
--- lambda/uploadLambda.ts
+++ lambda/uploadLambda.ts
@@ -1,63 +1,92 @@
-import { S3 } from 'aws-sdk';
-import {v4 as uuidv4} from 'uuid';
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { createPresignedPost } from '@aws-sdk/s3-presigned-post';
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+} from "@aws-sdk/client-s3";
 
-const client = new S3Client({ region: 'us-east-1' });
-// const s3 = new S3();
+const client = new S3Client({ region: "us-east-1" });
 
 export const handler = async (event: any): Promise<any> => {
-  if (!process.env.BUCKET_NAME){
+  if (!process.env.BUCKET_NAME) {
     return {
-          statusCode: 500,
-          body: JSON.stringify('Bucket name not configured'),
-        };
+      statusCode: 500,
+      body: JSON.stringify("Bucket name not configured"),
+    };
   }
 
   const bucketName: string = process.env.BUCKET_NAME;
-  console.log(`upload lambda invoked: ${bucketName}`)
-  console.log(`eventbody:${JSON.stringify(event)}`)
+  console.log(`upload lambda invoked: ${bucketName}`);
+  console.log(`eventbody:${JSON.stringify(event)}`);
   // Check if the event body is present
   try {
-    if(!event.body){
-      return{
+    
+    console.log(event.body)
+    if (!event.body) {
+      return {
         statusCode: 400,
-        body: JSON.stringify({message:'no file received'})
-      }
+        body: JSON.stringify({ message: "no file received" }),
+      };
     }
-  // const bodyBuffer=Buffer.from(event.body, event.isBase64Encoded ? 'base64' : 'utf8')
-  
-  const filename = `${uuidv4()}.mp3`
-      // Set the expiration time for the presigned URL (e.g., 15 minutes)
-      const urlExpiration = 15 * 60; // in seconds
-    
-      // Generate the presigned URL
-      // 
-    
-      const command = new PutObjectCommand({
-        Bucket: bucketName,
-        Key: filename,
-      ContentType: 'audio/mpeg',
-      });
-      
-      const presignedUrl = await getSignedUrl(client, command, { expiresIn: urlExpiration });
-      
-      // const presignedPost = await createPresignedPost(client, {
-      //   Bucket: bucketName,
-      //   Key: filename,
-      //   Expires: 3600, 
-      // });
+   
+    const { eventType, fileName } = JSON.parse(event.body);
+    if (!fileName) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: "no file name received" }),
+      };
+    }
+    if (!eventType) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: "no event type received" }),
+      };
+    }
+    // if (event.body.eventType != "get" || event.body.eventType != "update") {
+    //   return {
+    //     statusCode: 400,
+    //     body: JSON.stringify({ message: "no event type received" }),
+    //   };
+    // }
+    // Set the expiration time for the presigned URL (e.g., 15 minutes)
+    const urlExpiration = 15 * 60; // in seconds
+    let command;
+    // Generate the presigned URL
+    switch (eventType) {
+      case "get":
+        command = new GetObjectCommand({
+          Bucket: bucketName,
+          Key: fileName,
+        });
+        break;
+      case "upload":
+        command = new PutObjectCommand({
+          Bucket: bucketName,
+          Key: fileName,
+          // add dynamoDB meta data fields here.. e.g. user id, size, last updated, length, etc.
+        });
+        break;
+    }
+    if (!command) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: "no event type received" }),
+      };
+    }
+
+    const presignedUrl = await getSignedUrl(client, command, {
+      expiresIn: urlExpiration,
+    });
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ uploadUrl: presignedUrl, filename }),
+      body: JSON.stringify({ uploadUrl: presignedUrl, fileName }),
     };
   } catch (error) {
     console.error(error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'Could not generate presigned URL' }),
+      body: JSON.stringify({ error: "Could not generate presigned URL" }),
     };
   }
 };
